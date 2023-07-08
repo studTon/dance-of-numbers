78b416400e8bcba6cbdaeda0e974992a2b8adab0
studTon <evertonsdasilva1@gmail.com>
2023-07-07 23:03:54 -0300

    Create game library and adjust document format

--- game_functions.h
+++ game_functions.h
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "library.h"
 
-void title_bar(void){
+void title_bar(void)
+{
     int i = 0;
-    while(i < 32){
+    while (i < 32)
+    {
         printf("*");
         i += 1;
     }
     printf("\n");
 }
 
-void title(void) {
+void title(void)
+{
     printf("\n\nDANCE OF NUMBERS\n\n");
 }
 
-void menu(void) {
+void menu(void)
+{
     int option = 1;
 
-    while(option != 0) {
+    while (option != 0)
+    {
         system("@cls");
 
         title_bar();
@@ -34,17 +39,14 @@ void menu(void) {
         {
         case 0:
             break;
-        case 1: {
-            //play game function
+        case 1:
+        {
+            // play game function
             printf("OK");
             break;
         }
         default:
             break;
         }
-        
     }
-    
-    
 }
-
---
+++ game_library.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int sort_number(int max_number)
+{
+
+    int r;
+    srand(time(NULL));
+    r = (rand() % max_number) + 1;
+
+    return r;
+}
+
+bool guess(int guess_number)
+{
+    /*
+    Receive a number to verify if it's 
+    higher or lower than the sorted number
+    */
+}
