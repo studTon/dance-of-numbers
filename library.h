30c790803727991cb8d60102f2c82e604e5f6280
studTon <evertonsdasilva1@gmail.com>
2023-12-17 00:05:21 -0300

    Game functionality and settings

--- library.h
+++ library.h
@@ -2,6 +2,8 @@
     Game Library
 */
 
+int difficulty = 0; //Difficulty global variable
+
 void titleBar(void)
 {
     int i;
@@ -18,12 +20,12 @@ void title(void)
     printf("\n\n            Dance of Numbers\n\n");
     titleBar();
     printf("\n");
-    system("sleep 10s");
+    system("sleep 5s");
 }
 
 int defineSettings(void)
 {
-    int difficulty = 0;
+    
     system("clear");
     printf("Which difficulty do you wanna play?\n\n");
     printf("    0 - Easy\n");
@@ -31,13 +33,165 @@ int defineSettings(void)
     printf("    2 - Hard\n");
     printf("\n\n    ::");
     scanf("%d", &difficulty);
+    fflush(stdin);
+
     return difficulty;
 }
 
-void play(int diffOpt)
+void play(void)
 {
+    // Part of this code is based on the link: 
+    //https://www.geeksforgeeks.org/number-guessing-game-in-c/
+
     system("clear");
-    printf("\n\nOK\n\n");
+
+    int find, guess, num_guesses = 0;
+
+    //Seed random number generator
+    srand(time(NULL));
+
+    if (difficulty == 0)
+    {
+        find = rand() % 34;
+        
+            do {
+                system("clear");
+                printf("Guess a number between 0 and 33\n");
+
+                if (num_guesses > 9) {
+                    printf("\nYou Loose!\n");
+                    system("sleep 5s");
+                    break;
+                }
+        
+                // Input by user
+                scanf("%d", &guess);
+        
+                // When user guesses lower
+                // than actual number
+                if (guess > find)
+                {
+                    printf("Lower number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+ 
+                // When user guesses higher
+                // than actual number
+                else if (find > guess)
+        
+                {
+                    printf("Higher number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+        
+                // Printing number of times
+                // user has taken to guess
+                // the number
+                else{
+                    printf("You guessed the number in %d attempts!\n", num_guesses);
+                }
+                    
+ 
+            } while (guess != find);
+    }
+    else if(difficulty == 1)
+    {
+        find = rand() % 67;
+            do {
+                system("clear");
+                printf("Guess a number between 0 and 66\n");
+
+                if (num_guesses > 9) {
+                    printf("\nYou Loose!\n");
+                    system("sleep 5s");
+                    break;
+                }
+        
+                // Input by user
+                scanf("%d", &guess);
+        
+                // When user guesses lower
+                // than actual number
+                if (guess > find)
+                {
+                    printf("Lower number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+ 
+                // When user guesses higher
+                // than actual number
+                else if (find > guess)
+        
+                {
+                    printf("Higher number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+        
+                // Printing number of times
+                // user has taken to guess
+                // the number
+                else{
+                    printf("You guessed the number in %d attempts!\n", num_guesses);
+                }
+                    
+ 
+            } while (guess != find);
+    }
+    else if(difficulty == 2)
+    {
+        find = rand() % 101;
+            do {
+                system("clear");
+                printf("Guess a number between 0 and 100\n");
+
+                if (num_guesses > 9) {
+                    printf("\nYou Loose!\n");
+                    system("sleep 5s");
+                    break;
+                }
+        
+                // Input by user
+                scanf("%d", &guess);
+        
+                // When user guesses lower
+                // than actual number
+                if (guess > find)
+                {
+                    printf("Lower number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+ 
+                // When user guesses higher
+                // than actual number
+                else if (find > guess)
+        
+                {
+                    printf("Higher number please!\n");
+                    system("sleep 5s");
+                    num_guesses++;
+                }
+        
+                // Printing number of times
+                // user has taken to guess
+                // the number
+                else{
+                    printf("You guessed the number in %d attempts!\n", num_guesses);
+                }
+                    
+ 
+            } while (guess != find);
+    }
+    else if (difficulty < 0 && difficulty > 2)
+    {
+        difficulty = 0; //default
+        printf("Wrong input");
+    }
+    
     system("sleep 5s");
 }
 
@@ -45,7 +199,6 @@ void start(void)
 {
 
     int option = 0;
-    int diff = 0;
     while (option != 2)
     {
         system("clear");
@@ -60,14 +213,14 @@ void start(void)
         {
             case 0:
             {
-                diff = defineSettings();
+                difficulty = defineSettings();
             }break;
-            case 1: play(diff);break;
+            case 1: play();break;
             case 2:
             {
                 system("clear");
                 printf("        GOING AWAY HAHAHA...\n\n");
-                system("sleep 10s");
+                system("sleep 3s");
                 
             }break;
         }
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 //This code is free forever :)
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "library.h"
 
 int main(int argc, char*argv[]){
